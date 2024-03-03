d6dd5ba5d4a7df999279413c365f08af04bb6606
rensawamo <ren0112ngzk@gmail.com>
2024-03-03 17:40:06 +0900

    flavor firebase 設定

--- lib/configs/componets/loading_widget.dart
+++ lib/configs/componets/loading_widget.dart
@@ -26,4 +26,3 @@ class LoadingWidget extends StatelessWidget {
     );
   }
 }
-ds
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 FutureOr<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
