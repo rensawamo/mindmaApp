326a8485ebf42667533a3bc7e2761a0c84c7afae
rensawamo <ren0112ngzk@gmail.com>
2024-04-08 14:50:43 +0900

    ios build

---
+++ ios/scripts/dart_define.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+echo $DART_DEFINE | tr ',' '\n' | while read line; 
+do
+  echo $line | base64 -d | tr ',' '\n' | xargs -I@ bash -c "echo @ | grep 'FLAVOR' | sed 's/.*=//'"
+  echo $flavor
+done | (
+  read flavor
+  echo "#include \"$flavor.xcconfig\"" > $SRCROOT/Flutter/DartDefine.xcconfig
+)
